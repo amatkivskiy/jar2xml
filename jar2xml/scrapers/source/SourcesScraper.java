7a8caa40ea2f25aaa4ce37a75d03b5828db80f3c
amatkivskiy <andriy.matkivskiy@gmail.com>
2015-06-22 21:44:53 +0300

    Fixed search for methods in class.

--- jar2xml/scrapers/source/SourcesScraper.java
+++ jar2xml/scrapers/source/SourcesScraper.java
@@ -4,13 +4,13 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.lang.reflect.Type;
-import java.util.Arrays;
 import java.util.List;
 
 import japa.parser.JavaParser;
 import japa.parser.ParseException;
 import japa.parser.ast.CompilationUnit;
 import japa.parser.ast.body.BodyDeclaration;
+import japa.parser.ast.body.ConstructorDeclaration;
 import japa.parser.ast.body.MethodDeclaration;
 import japa.parser.ast.body.Parameter;
 import japa.parser.ast.body.TypeDeclaration;
@@ -33,49 +33,47 @@ public class SourcesScraper implements IDocScraper {
   }
 
   @Override public String[] getParameterNames(ClassNode asm, String name, Type[] ptypes, boolean isVarArgs) {
-    boolean isInner = false;
     String fileName = sourcesDir.getPath() + "/";
+
     if (asm.name.contains("$")) {
-      isInner = true;
-      fileName += asm.name.substring(0, asm.name.indexOf("$"));
+      return EMPTY_RESPONSE;
+//      fileName += asm.name.substring(0, asm.name.indexOf("$"));
     } else {
       fileName += asm.name;
     }
     fileName += ".java";
 
-    if (isInner) return EMPTY_RESPONSE;
-
     try {
-      MethodDeclaration method = getMethodForName(fileName, name, ptypes);
-      if (method == null) {
-        System.err.println("Unable to find method \"" + name + "\" with params : " + Arrays.toString(ptypes));
+      List<Parameter> params = getParamsForMethodName(fileName, name, ptypes);
+      if (params == null) {
         return EMPTY_RESPONSE;
       }
-
-      return getMethodParamNames(method);
+      return getMethodParamNames(params);
     } catch (FileNotFoundException e) {
       System.err.println(String.format("File {%s} for class {%s} not found.", fileName, asm.name));
     } catch (ParseException e) {
       System.err.println(String.format("Failed to parse file {%s} for class {%s}.", fileName, asm.name));
+    } catch (StringIndexOutOfBoundsException e) {
+      System.err.println("Something went wrong. Need to investigate. File: " + fileName);
     }
 
     return EMPTY_RESPONSE;
   }
 
-  private String[] getMethodParamNames(MethodDeclaration method) {
-    if (method.getParameters() == null) {
-      return new String[0];
-    } else{
-      String[] results = new String[method.getParameters().size()];
+  private String[] getMethodParamNames(List<Parameter> parameters) {
+    if (parameters == null) {
+      return EMPTY_RESPONSE;
+    } else {
+      String[] results = new String[parameters.size()];
       for (int i = 0; i < results.length; i++) {
-        results[i] = method.getParameters().get(i).getId().getName();
+        results[i] = parameters.get(i).getId().getName();
       }
 
       return results;
     }
   }
 
-  private MethodDeclaration getMethodForName(String file, String name, Type[] methodParams) throws FileNotFoundException, ParseException {
+  private List<Parameter> getParamsForMethodName(String file, String name, Type[] methodParams) throws FileNotFoundException, ParseException {
     CompilationUnit javaFile = JavaParser.parse(new FileInputStream(file));
 
     TypeDeclaration clazz = javaFile.getTypes().get(0);
@@ -83,31 +81,86 @@ public class SourcesScraper implements IDocScraper {
     for (BodyDeclaration bodyDeclaration : clazz.getMembers()) {
       if (bodyDeclaration instanceof MethodDeclaration) {
         MethodDeclaration method = (MethodDeclaration) bodyDeclaration;
-        if (method.getName().equals(name)) {
-          if (method.getParameters() == null && methodParams.length != 0) {
-            continue;
-          }
 
-          if (checkIfParamsMatches(method.getParameters(), methodParams)) {
-            return method;
-          }
+        if (checkIfMethodMatches(method.getName(), method.getParameters(), name, methodParams)) {
+          System.out.println("Found " + clazz.getName() + "." + method.getName() + "(" + getMethodParamsString(method.getParameters()) + ")");
+          return method.getParameters();
+        }
+      } else if (bodyDeclaration instanceof ConstructorDeclaration) {
+        ConstructorDeclaration constructor = (ConstructorDeclaration) bodyDeclaration;
+
+        if (checkIfMethodMatches(constructor.getName(), constructor.getParameters(), name, methodParams)) {
+          System.out.println("Found " + clazz.getName() + "." + constructor.getName() + "(" + getMethodParamsString(constructor.getParameters()) + ")");
+          return constructor.getParameters();
         }
       }
     }
 
+    System.err.println(String.format("Method {%s(%s)} not found in {%s}", name, getMethodParamsString(methodParams), file));
+
     return null;
   }
 
-  private boolean checkIfParamsMatches(List<Parameter> methodParams, Type[] ptypes) {
+  private String getMethodParamsString(Type[] params) {
+    StringBuilder builder = new StringBuilder("");
+
+    if (params.length == 0) {
+      return builder.toString();
+    }
+
+    for (int i = 0; i < params.length; i++) {
+      builder.append(params[i].toString());
+
+      if (i != params.length - 1) {
+        builder.append(",");
+      }
+    }
+
+    return builder.toString();
+  }
+
+  private String getMethodParamsString(List<Parameter> params) {
+    StringBuilder builder = new StringBuilder("");
+
+    if (params == null) {
+      return builder.toString();
+    }
+
+    for (Parameter param : params) {
+      builder.append(param.getId().getName());
+
+      if (params.indexOf(param) != params.size() - 1) {
+        builder.append(",");
+      }
+    }
+
+    return builder.toString();
+  }
+
+  private boolean checkIfMethodMatches(String methodName, List<Parameter> methodParameters, String targetMethodName,
+      Type[] targetMethodParams) {
+    if (methodName.equals(targetMethodName)) {
+      if (methodParameters == null && targetMethodParams.length != 0) {
+        return false;
+      }
+
+      if (checkIfParamsMatches(methodParameters, targetMethodParams)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private boolean checkIfParamsMatches(List<Parameter> methodParams, Type[] pTypes) {
     for (Parameter parameter : methodParams) {
       boolean doesMatches = false;
 
-      for (Type ptype : ptypes) {
+      for (Type pType : pTypes) {
         //TODO: optimize this ugly algo!!!
         //TODO: Maybe there is need to check whether full type names match each other rather than only short type name
         //TODO:  like Request and com.squareup.picasso.Request
-        String ptypeWithDots = ptype.toString().replace("$", ".");
-        doesMatches = doesMatches || ptypeWithDots.contains(parameter.getType().toString());
+        String pTypeWithDots = pType.toString().replace("$", ".");
+        doesMatches = doesMatches || pTypeWithDots.contains(parameter.getType().toString());
       }
 
       if (!doesMatches) {
